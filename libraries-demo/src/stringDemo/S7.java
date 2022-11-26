e6db4ea4414756a3a550b04e170c619fec605942
sathish <sathish.n.yadav@gmail.com>
2022-11-26 13:45:28 +0530

    programs on arrays

--- libraries-demo/src/stringDemo/S7.java
+++
@@ -1,8 +0,0 @@
-package stringDemo;
-
-public class S7 {
-	public static void main(String[] args) {
-		String s = "hello";
-		System.out.println(s.charAt(s.length()));// StringIndexOutOfBoundsException
-	}
-}
